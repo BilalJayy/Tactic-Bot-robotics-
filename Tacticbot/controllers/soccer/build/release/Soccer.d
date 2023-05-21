09bdd0e90d2b53261f02b2201a29842c66d7e09f
BilalJayy <117118875+BilalJayy@users.noreply.github.com>
2023-05-21 05:15:10 -0700

    Add files via upload

---
+++ Tacticbot/controllers/soccer/build/release/Soccer.d
@@ -0,0 +1,24 @@
+build/release/Soccer.o: Soccer.cpp Soccer.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/Robot.hpp \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/managers/include/RobotisOp2GaitManager.hpp \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/managers/include/RobotisOp2MotionManager.hpp \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/managers/include/RobotisOp2VisionManager.hpp \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/ColorFinder.h \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/Point.h \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/Image.h \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/minIni.h \
+ E:/Webots/Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/src/minIni/minIni.h \
+ E:/Webots/Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/src/minIni/minGlue.h \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/Image.h \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/ImgProcess.h \
+ E:\Webots\Webots/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Framework/include/Point.h \
+ E:\Webots\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/Device.hpp \
+ E:/Webots/Webots/include/controller/c/webots/types.h \
+ E:\Webots\Webots/include/controller/cpp/webots/Camera.hpp \
+ E:/Webots/Webots/include/controller/c/webots/camera_recognition_object.h \
+ E:\Webots\Webots/include/controller/cpp/webots/Gyro.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/LED.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/Motor.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/Speaker.hpp
---
+++ Tacticbot/controllers/soccer/build/release/main.d
@@ -0,0 +1,2 @@
+build/release/main.o: main.cpp Soccer.hpp \
+ E:\Webots\Webots/include/controller/cpp/webots/Robot.hpp
